65666ddef93799ed1fa994065c915718c65805eb
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-02-15 13:27:10 -0800

    setting up stdin in play.js

--- play.js
+++ play.js
@@ -1,3 +1,19 @@
 const { connect } = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
 
+// runs when we receive input from our keyboard
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
